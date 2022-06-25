92f750a27301a7a8ce4ba3148295976cc03aada8
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-06-25 22:17:40 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
+
+const cors = require('cors');
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntqc6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        const usersCollection = client.db("inventoryManagement").collection("users");
+        
+        
+    } 
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Server is running port ${port}`)
+    console.log(`Server is running port ${port}`)
 })
